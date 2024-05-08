70200bf2e475a4d911331734800b321079b135fc
LongDDH <48487369+DDHLong@users.noreply.github.com>
2024-05-08 23:40:12 +0700

    add kinde auth and customer testimonial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import CustomerTestimonial from "@/components/CustomerTestimonial";
+import { Icon } from "@/components/Icon";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import { Check, Star } from "lucide-react";
@@ -97,6 +99,28 @@ export default function Home() {
               />
               <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      {/* value proposition section*/}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              What our{" "}
+              <span className="relative px-2">
+                customers{" "}
+                <Icon.underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500" />
+              </span>{" "}
+              say
+            </h2>
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <CustomerTestimonial name="Jonathan" />
+            <CustomerTestimonial name="Long" />
           </div>
         </MaxWidthWrapper>
       </section>
